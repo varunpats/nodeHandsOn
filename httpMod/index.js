e28b197e7b19cf2e9d0899ced0a45525aadd8e70
varunpats <varunpatil2502@gmail.com>
2024-03-27 23:27:17 +0530

    http module

---
+++ httpMod/index.js
@@ -0,0 +1,9 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.end("Hello World!");
+})
+
+server.listen(8000, "127.0.0.1", () => {
+    console.log("Server listening")
+})
