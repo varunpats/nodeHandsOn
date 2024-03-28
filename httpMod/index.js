38cadde47d8f49c63af81845aec47ccfddc6f1d0
varunpats <varunpatil2502@gmail.com>
2024-03-28 22:45:12 +0530

    Node Routing

--- httpMod/index.js
+++ httpMod/index.js
@@ -1,7 +1,16 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    res.end("Hello World!");
+    if (req.url == '/') {
+        res.end("Hello World!");
+    } else if (req.url == '/about') {
+        res.end("About");
+    } else if (req.url == '/contact') {
+        res.end("Contact!");
+    } else {
+        res.writeHead('404');
+        res.end("Page not found!");
+    }
 })
 
 server.listen(8000, "127.0.0.1", () => {
