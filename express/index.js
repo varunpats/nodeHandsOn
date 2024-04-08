bbc1fd99a8bd55425fe4ff9c22a258acfb11bc73
varunpats <varunpatil2502@gmail.com>
2024-04-08 23:07:15 +0530

    Adding static content using node

--- express/index.js
+++ express/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
+
+const staticPath = path.join(__dirname, "/public");
+
+app.use(express.static(staticPath))
 
 app.get('/', (req, res) => {
     res.send("Hello world")
