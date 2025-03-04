20ea95bbd615c6f6175422e61d38d70dc1212bd6
varunpats <varunpatil2502@gmail.com>
2025-03-04 20:53:13 +0530

    basic path module methods

--- pathModule/index.js
+++ pathModule/index.js
@@ -1,6 +1,11 @@
 const path = require('path');
 
-console.log(path.dirname('C:/Users/Varun/Desktop/nodeHandsOn/pathModule/index.js'));
-console.log(path.extname('C:/Users/Varun/Desktop/nodeHandsOn/pathModule/index.js'));
-console.log(path.basename('C:/Users/Varun/Desktop/nodeHandsOn/pathModule/index.js'));
-console.log(path.parse('C:/Users/Varun/Desktop/nodeHandsOn/pathModule/index.js'));
+// console.log(path.dirname('C:/Users/Varun/Desktop/nodeHandsOn/pathModule/index.js'));
+// console.log(path.extname('C:/Users/Varun/Desktop/nodeHandsOn/pathModule/index.js'));
+// console.log(path.basename('C:/Users/Varun/Desktop/nodeHandsOn/pathModule/index.js'));
+// console.log(path.parse('C:/Users/Varun/Desktop/nodeHandsOn/pathModule/index.js'));
+
+console.log(__dirname);
+console.log(__filename);
+console.log(path.join("varun","patil"));
+
